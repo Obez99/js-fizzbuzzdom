ea6fcea7b9cad1555a7e9fb71841e2c9dee06cc8
Obez99 <giuliogx@live.it>
2021-10-12 16:42:54 +0200

    Creates grid

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,9 @@
-const container = document.querySelector(".container");
+const container = document.querySelector(".my_container");
 const row = document.createElement("div");
-let col = document.createElement("div");
-row.classList.add("row");
-col.classList.add("col");
-container.append(row);
 
 for (let i = 1; i <= 100; i++){
-  row.append(col);
+  let col = document.createElement("div");
+  col.className = "fizz_container";
+  container.append(col);
 
-  //col = `<div class="col">${i}</div>`;
-  //row.innerHTML += col;
 }
