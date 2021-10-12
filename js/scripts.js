099e3e471095a3996081c3073724b321f73cd072
Obez99 <giuliogx@live.it>
2021-10-12 17:12:54 +0200

    Completes exercise

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,19 @@ for (let i = 1; i <= 100; i++){
   col.className = "fizz_container";
   container.append(col);
   col.textContent = i;
+
+  if(i % 3 === 0){
+  col.textContent = "fizz";
+  col.style.backgroundColor = "rgb(" + 11 + "," + 214 + "," + 161 + ")";
+  }
+
+  if(i % 5 === 0){
+  col.textContent = "buzz";
+  col.style.backgroundColor = "rgb(" + 255 + "," + 209 + "," + 102 + ")";
+  }
+
+  if(i % 3 === 0 && i % 5===0){
+  col.textContent = "fizzbuzz";
+  col.style.backgroundColor = "rgb(" + 240 + "," + 70 + "," + 111 + ")";
+  }
 }
